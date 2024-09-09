855e4899478c2ebdf509f957cf103bdc0dbafccf
Harshitha <harshithakarnati2535@gmail.com>
2024-09-09 14:23:42 -0400

    worked on coding for product fetch in shop page

--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -6,5 +6,6 @@ const router = require('express').Router()
 router.get('/get-categories',homeController.get_categories)
 router.get('/get-products',homeController.get_products)
 router.get('/price-range-latest-products',homeController.price_range_product)
+router.get('/query-products',homeController.query_products)
 
 module.exports = router
