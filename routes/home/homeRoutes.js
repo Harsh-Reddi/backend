153ab438600f9ff8e049ded957d146e01d94ed21
Harshitha <harshithakarnati2535@gmail.com>
2024-10-22 11:20:10 -0400

    added dynamic data to the details page

--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -7,5 +7,6 @@ router.get('/get-categories',homeController.get_categories)
 router.get('/get-products',homeController.get_products)
 router.get('/price-range-latest-products',homeController.price_range_product)
 router.get('/query-products',homeController.query_products)
+router.get('/product-details/:slug',homeController.product_details)
 
 module.exports = router
