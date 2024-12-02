a20661af09058f98f9a2e174e0f70d4df0c0ac3e
Harsh-Reddi <Harshithareddi14@gmail.com>
2024-12-02 15:06:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const socket = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://frontend-lilac-nine.vercel.app', 'https://dashboard-sand-chi-64.vercel.app'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://frontend-lilac-nine.vercel.app', 'dashboard-phi-azure-89.vercel.app
+'],
 
     credentials : true
 }))
